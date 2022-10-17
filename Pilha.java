b8c4a57a8ab6c6a0389682186e7d8048028f20fb
Andressa Farkas <48599122+andressafarkas@users.noreply.github.com>
2022-10-17 16:19:41 -0300

    Updated Pilha method

--- Pilha.java
+++ Pilha.java
@@ -3,10 +3,10 @@ import java.util.EmptyStackException;
 public class Pilha {
 
     private class Node {
-        public Integer element;
+        public char element;
         public Node next;
 
-        public Node(Integer element) {
+        public Node(char element) {
             this.element = element;
             next = null;
         }
@@ -14,37 +14,35 @@ public class Pilha {
 
     private Node topo;
     private int count;
-    private Pilha p;
 
     public Pilha() {
-        p = new Pilha();
         topo = null;
         count = 0;
     }
 
-       public void push (Integer e){
+       public void push (char e){
         Node aux = new Node(e);
         aux.next = topo;
         topo = aux;
-        count++;
+        count ++;
     }
 
-    public Integer pop(){
+    public char pop(){
         if(count == 0){
             throw new EmptyStackException();
         }
-        count--;
-        Integer aux = topo.element;
+        count --;
+        char aux = topo.element;
         topo = topo.next;
         return aux;
     }
 
-    public Integer top() {
-        if(p.isEmpty()){
+    public char top() {
+        if(isEmpty()){
             throw new EmptyStackException();
         }
         return topo.element;
-        }
+    }
 
     public int size() {
         return count;
